1b3e51799f7c34081d003d4d0ddfab06c824b823
Rohen Peterson <rohen.peterson@gmail.com>
2012-12-29 16:30:50 -0800

    adding file

---
+++ generate-sinatra.rb
@@ -0,0 +1,17 @@
+def exec_with_status(cmd, inputs_hash)
+  puts "- `rvm use #{inputs_hash[:rvm]}; #{cmd}`"
+  `rvm use #{inputs_hash[:rvm]}; #{cmd}`
+end
+
+DEFAULTS = {
+  rvm: '1.9.3-head'
+}
+
+inputs = {}
+
+exec_with_status("touch app.rb", inputs)
+exec_with_status("touch config.rb", inputs)
+exec_with_status("touch Gemfile", inputs)
+exec_with_status("mkdir public", inputs)
+exec_with_status("mkdir views", inputs)
+
